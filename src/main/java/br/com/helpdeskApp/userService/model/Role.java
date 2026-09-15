66da538109b318f209273d1e07bda794ea97045e
Pedro Paulo Silva Santana Ferreira <pedro.ferreira@solutis.com.br>
2026-09-15 11:51:16 -0300

    feat: add database entity and migration

---
+++ src/main/java/br/com/helpdeskApp/userService/model/Role.java
@@ -0,0 +1,7 @@
+package br.com.helpdeskApp.userService.model;
+
+public enum Role {
+    CLIENT,
+    TECHNICIAN,
+    ADMIN;
+}
---
+++ src/main/java/br/com/helpdeskApp/userService/model/User.java
@@ -0,0 +1,36 @@
+package br.com.helpdeskApp.userService.model;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@AllArgsConstructor 
+@NoArgsConstructor
+@Getter 
+@Setter  
+public class User {
+    @Id 
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    private boolean active;
+    private LocalDateTime createdAt;
+
+}
