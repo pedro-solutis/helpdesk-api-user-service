8191a4eebe417ab85a6475e1e4d54bdb9b2c310e
Pedro Paulo Silva Santana Ferreira <pedro.ferreira@solutis.com.br>
2026-09-15 23:51:11 -0300

    test: create user controller structure

---
+++ src/test/java/br/com/helpdeskApp/userService/controller/UserControllerTest.java
@@ -0,0 +1,30 @@
+package br.com.helpdeskApp.userService.controller;
+
+import org.junit.jupiter.api.Test;
+
+public class UserControllerTest {
+    @Test
+    void testCreateUser() {
+
+    }
+
+    @Test
+    void testDeleteUser() {
+
+    }
+
+    @Test
+    void testGetAllUsers() {
+
+    }
+
+    @Test
+    void testGetUserById() {
+
+    }
+
+    @Test
+    void testUpdateUser() {
+
+    }
+}
