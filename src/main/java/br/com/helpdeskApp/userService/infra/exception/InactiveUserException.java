5f24ddfb9f0a9e4ece9c7750e5e8a2645249c811
Pedro Paulo Silva Santana Ferreira <pedro.ferreira@solutis.com.br>
2026-09-19 12:22:29 -0300

    feat(exception) implemtn inactive user exception

--- src/main/java/br/com/helpdeskApp/userService/infra/exception/InactiveUserException.java
+++ src/main/java/br/com/helpdeskApp/userService/infra/exception/InactiveUserException.java
@@ -1,5 +1,13 @@
 package br.com.helpdeskApp.userService.infra.exception;
 
-public class InactiveUserException {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class InactiveUserException extends RuntimeException{
+
+    public InactiveUserException(String message) {
+        super(message);
+    }
 
 }
