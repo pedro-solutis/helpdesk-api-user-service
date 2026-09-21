a648a840fba392d0c37ad79fb31928d3ed9d4c4d
Pedro Paulo Silva Santana Ferreira <pedro.ferreira@solutis.com.br>
2026-09-21 10:56:13 -0300

    feat(swagger): permit gateway access to openapi docs and swagger ui

--- src/main/java/br/com/helpdeskApp/userService/UserServiceApplication.java
+++ src/main/java/br/com/helpdeskApp/userService/UserServiceApplication.java
@@ -3,6 +3,10 @@ package br.com.helpdeskApp.userService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(servers = { @Server(url = "http://localhost:8080/api") })
 @SpringBootApplication
 public class UserServiceApplication {
 
