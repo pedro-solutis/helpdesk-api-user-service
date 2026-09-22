0eed4930630cb901053b189baad959abdcba63b7
Pedro Paulo Silva Santana Ferreira <pedro.ferreira@solutis.com.br>
2026-09-21 22:40:49 -0300

    refactor(config): externalize application settings to environment variables

--- src/main/java/br/com/helpdeskApp/userService/UserServiceApplication.java
+++ src/main/java/br/com/helpdeskApp/userService/UserServiceApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.servers.Server;
 
-@OpenAPIDefinition(servers = { @Server(url = "http://localhost:8080/api"), @Server(url = "http://localhost:8081") })
+@OpenAPIDefinition(servers = { @Server(url = "${GATEWAY_URL:http://localhost:8080/api}", description = "Gateway API"), @Server(url = "${LOCAL_URL:http://localhost:8081}", description = "Local Server") })
 @SpringBootApplication
 public class UserServiceApplication {
 
